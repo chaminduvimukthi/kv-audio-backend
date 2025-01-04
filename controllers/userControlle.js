83a9992e880714ceb30983b135e4b1c6078c4aa6
chamindu vimukthi <chaminduvimukthi7@gmail.com>
2025-01-04 10:42:41 +0530

    index.js and userControlle files implemented dotenv file👀

--- controllers/userControlle.js
+++ controllers/userControlle.js
@@ -1,7 +1,9 @@
 import User from '../model/user.js'
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 export function registerUser(req, res) {
   const data = req.body;
@@ -40,7 +42,7 @@ export function loginUser(req, res) {
           email :user.email,
           role : user.role
 
-        },"kv-secret-89!")
+        },process.env.SECRET_PASS)
         res.json({ message: "Login successful" ,token:token});
       } else {
         res.status(401).json({ error: "Login failed" });
--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 
   if (token != null) {
     token = token.replace("Bearer ", ""); 
-    jwt.verify(token, "kv-secret-89!", (err, decoded) => {
+    jwt.verify(token, process.env.SECRET_PASS, (err, decoded) => {
 
       if(!err){
         req.user = decoded;
